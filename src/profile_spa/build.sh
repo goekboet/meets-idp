35a8fa40ed873f2c05f48392d2930dcaa9c610e5
Erik Göök <erikgook@gmail.com>
2020-11-15 17:02:33 +0100

    Prepare dockerization

--- src/profile_spa/build.sh
+++ src/profile_spa/build.sh
@@ -7,4 +7,10 @@ cp src/index.js ../ids/wwwroot/profile_spa/index.js
 echo "Copy profile_spa.css"
 cp src/profile_spa.css ../ids/wwwroot/profile_spa/profile_spa.css
 
-ls -la ../ids/wwwroot/profile_spa
+echo "Copy favicon"
+cp favicon.ico ../ids/wwwroot/favicon.ico
+
+echo "Copy materialize"
+cp -r materialize ../ids/wwwroot
+
+tree --du ../ids/wwwroot
